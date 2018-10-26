8517a9824cc4ebfc1e0f35a4a3958137583b6f8e
zach calcagno <zcalcagno2480@jh-mba158-02.local>
2018-10-26 14:22:30 -0500

    Initial Commit

---
+++ Final calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Final calculator
+//
+//  Created by zach calcagno on 10/26/18.
+//  Copyright © 2018 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
