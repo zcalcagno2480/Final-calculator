5ebaae68ac54560f753669fa06ff9f2ec7b79226
zach calcagno <zcalcagno2480@jh-mba158-02.d214.org>
2018-10-30 14:56:48 -0500

    stretch two

--- Final calculator/ViewController.swift
+++ Final calculator/ViewController.swift
@@ -15,15 +15,44 @@ class ViewController: UIViewController {
     @IBOutlet weak var DesiredFinalGrade: UITextField!
     @IBOutlet weak var enterPerTheFinalIsWorth: UILabel!
     @IBOutlet weak var perTheFinaLIWorth: UITextField!
+    @IBOutlet weak var gradeNeeded: UILabel!
+    @IBOutlet weak var askForHelpLable: UILabel!
+    
+
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         
+
         
     }
     @IBAction func whenButtonPressed(_ sender: Any) {
+    let gradeC = Double(currentGrade.text!)!
+    let gradeW = Double(DesiredFinalGrade.text!)!
+    let weight = Double(perTheFinaLIWorth.text!)!
+    let  decimalPercent = weight / 100.00
+    let percentLeft = 1 - decimalPercent
+    let gradeN = (gradeW - (percentLeft * gradeC)) / decimalPercent
+        gradeNeeded.text = "\(gradeN)"
         
-    }
+        
+        
+        if gradeN > 100 {
+            view.backgroundColor = UIColor.red
+        } else if gradeN < 100 {
+           view.backgroundColor = UIColor.green
+        }
+
+        if gradeN > 100 {
+            
+            askForHelpLable.text = (" You Shold Ask Your Teacher For Extra Credit ")
+        } else if gradeN < 100{
+            askForHelpLable.text = (" ")
+        }
+        
+}
     
     
 
